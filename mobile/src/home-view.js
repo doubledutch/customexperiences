edabe99ebd9436f8124ea74e7e828a980babba5d
Eddie Stamps <estamps@doubledutch.me>
2018-10-15 14:49:20 -0700

    Test

--- mobile/src/home-view.js
+++ mobile/src/home-view.js
@@ -122,7 +122,7 @@ export default class HomeView extends Component {
     return (
       <View style={{flex: 1}}>
         {this.props.version ? null : <TitleBar title={client.currentEvent.name} client={client} signin={this.signin} />}
-        {this.state.componentConfigs.length ? null : <LoadingView logInFailed={this.state.logInFailed} isLaunch={this.props.version}/>}
+        {this.state.componentConfigs ? null : <LoadingView logInFailed={this.state.logInFailed} isLaunch={this.props.version}/>}
         <ConfigurableScroll componentConfigs={this.state.componentConfigs} handleScroll={this.handleScroll}/>
         {this.props.version ? <TouchableOpacity disabled={this.state.isDisabled} onPress={() => client.dismissLandingPage(false)} style={this.state.isDisabled ? s.launchButtonGray : s.launchButton}><Text style={s.launchButtonText}>{this.state.isDisabled ? "Scroll down to enter" : "Take me to the Event"}</Text></TouchableOpacity> : null}
       </View>
